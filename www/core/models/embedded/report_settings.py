700745cafb3be59d5d6da9e3f280dc5f832f545e
Thomas Durin <thomas@naeka.fr>
2013-11-29 16:03:05 +0100

    Add a list of supported fonts for pdf exports

--- www/core/models/embedded/report_settings.py
+++ www/core/models/embedded/report_settings.py
@@ -3,6 +3,8 @@
 from django.conf import settings
 from mongoengine import EmbeddedDocument, fields
 
+from core.pdf.colors import SUPPORTED_FONTS
+
 
 __all__ = (
     'ReportSettings',
@@ -12,7 +14,7 @@ __all__ = (
 class ReportSettings(EmbeddedDocument):
 
     """ReportSettings stores various informations used to customize reports"""
-    font_name = fields.StringField(required=True, default='Bariol')
+    font_name = fields.StringField(required=True, default='Bariol', choices=SUPPORTED_FONTS)
     font_size = fields.IntField(required=True, default=10)
     base_color = fields.StringField()
     force_bw = fields.BooleanField(required=True, default=False)
