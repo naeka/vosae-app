e8ee630b7bd65672435923630547d172a146395f
Maxime Gaillard <maxime@naeka.fr>
2014-01-08 22:59:33 +0100

    Send an email to unactive user after one day
    Signed-off-by: Maxime Gaillard <maxime@naeka.fr>

--- www/account/tasks.py
+++ www/account/tasks.py
@@ -1,6 +1,16 @@
 # -*- coding:Utf-8 -*-
 
-from celery.task import task
+import datetime
+
+from django.utils.timezone import now
+from django.template.loader import render_to_string
+from django.template import Context
+from django.conf import settings
+from django.core.mail import EmailMessage, get_connection
+
+from celery.task import task, periodic_task
+from celery.schedules import crontab
+
 from vosae_utils import respects_language
 
 
@@ -8,3 +18,25 @@ from vosae_utils import respects_language
 @respects_language
 def user_send_activation_email(user):
     user.send_activation_email()
+
+@periodic_task(run_every=crontab(minute=0, hour='*/1'))
+@respects_language
+def user_send_inactive_email():
+    from account.models import User
+
+    for user in User.objects.filter(is_active=False, date_joined__gte=now() - datetime.timedelta(days=1, hours=1), date_joined__lte=now() - datetime.timedelta(days=1)):
+
+        context = {
+            'user': user,
+            'site': {'name': settings.SITE_NAME, 'url': settings.SITE_URL}
+        }
+        # Send mails
+        connection = get_connection()
+        plaintext_context = Context(autoescape=False)  # HTML escaping not appropriate in plaintext
+        subject = render_to_string('account/emails/inactive_email_subject.txt', context, plaintext_context)
+        subject = ''.join(subject.splitlines())
+        text_body = render_to_string('account/emails/inactive_email_message.txt', context, plaintext_context)
+
+        message = EmailMessage(subject=subject, from_email="maxime@vosae.com", to=[user.email], body=text_body, connection=connection)
+        message.preserve_recipients = False # Useful for Mandrill
+        message.send()
