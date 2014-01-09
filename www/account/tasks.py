4fa7cc75deff7efda8d7fa751ae187fc349a27ed
Maxime Gaillard <maxime@naeka.fr>
2014-01-09 10:50:02 +0100

    Send an email to new user after one week
    Signed-off-by: Maxime Gaillard <maxime@naeka.fr>

--- www/account/tasks.py
+++ www/account/tasks.py
@@ -40,3 +40,25 @@ def user_send_inactive_email():
         message = EmailMessage(subject=subject, from_email="maxime@vosae.com", to=[user.email], body=text_body, connection=connection)
         message.preserve_recipients = False # Useful for Mandrill
         message.send()
+
+@periodic_task(run_every=crontab(minute=0, hour='*/1'))
+@respects_language
+def user_send_after_one_week_email():
+    from account.models import User
+
+    for user in User.objects.filter(is_active=True, date_joined__gte=now() - datetime.timedelta(days=7, hours=1), date_joined__lte=now() - datetime.timedelta(days=7)):
+
+        context = {
+            'user': user,
+            'site': {'name': settings.SITE_NAME, 'url': settings.SITE_URL}
+        }
+        # Send mails
+        connection = get_connection()
+        plaintext_context = Context(autoescape=False)  # HTML escaping not appropriate in plaintext
+        subject = render_to_string('account/emails/after_one_week_email_subject.txt', context, plaintext_context)
+        subject = ''.join(subject.splitlines())
+        text_body = render_to_string('account/emails/after_one_week_email_message.txt', context, plaintext_context)
+
+        message = EmailMessage(subject=subject, from_email="maxime@vosae.com", to=[user.email], body=text_body, connection=connection)
+        message.preserve_recipients = False # Useful for Mandrill
+        message.send()
